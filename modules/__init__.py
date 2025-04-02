be707f30e0e3bcaf9e48f84bc7b186b07a27a41e
timer <timer@example.com>
2025-04-02 18:47:08 +0200

    class update

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from MinesGame.data_storage import data
+from .handlers import user_input_handler, position_handler, forced_position_handler, menu_handler
+
+
+    
+
+__all__ = ["vector", "handlers", "position_2D", forced_position_handler, menu_handler, user_input_handler, position_handler, ]
