8bd9ed0b88b785c186905f7c8731e1402fe6c18a
Bobo <spocky@post.cz>
2025-04-09 22:01:30 +0200

    Revert "...."
    This reverts commit d112e6808c9f8fb57ba6cac727e6471e0b6840d6.

--- crypto_main/metric.py
+++ crypto_main/metric.py
@@ -1,30 +1,20 @@
-
+from crypto_main.data_storage import data
 
 
 class Metric:
-    def __init__(self, data_storage):
-        self._data_storage = data_storage
-        self._metric_data = []
+    def __init__(self, data):
 
-
-    def _initialize_metrics(self):
         self._metric_data = [
-                {"Current vector 2D": self._data_storage.get_vector(), },
-                {"Current position 2D": self._data_storage.get_position_2D()},
-                {"Minimum X , Y": (self._data_storage.get_position_modifier()["x_min"], self._data_storage.get_position_modifier()["y_min"])},
-                {"Maximum X , Y": (self._data_storage.get_position_modifier()["x_max"], self._data_storage.get_position_modifier()["y_max"])},
-                {"Forced X , Y":  (self._data_storage.get_position_modifier()["x_start"], self._data_storage.get_position_modifier()["y_start"])},
-                {"Current menu": self._data_storage.get_menu_position()},
-                {"Previous menu": self._data_storage.get_last_menu_position()},
+                {"Current vector 2D": data.get_vector(),},
+                {"Current position 2D": data.get_position_2D()},
+                {"Minimum X , Y": (data.get_position_modifier()["x_min"], data.get_position_modifier()["y_min"])},
+                {"Maximum X , Y": (data.get_position_modifier()["x_max"], data.get_position_modifier()["y_max"])},
+                {"Forced X , Y":  (data.get_position_modifier()["x_start"], data.get_position_modifier()["y_start"])},
+                {"Current menu": data.get_menu_position()},
+                {"Previous menu": data.get_last_menu_position()},
         ]
 
 
-
-    def set_data_storage(self, data_storage):
-        self._data_storage = data_storage
-        self._initialize_metrics()
-        
-        
     def set_metric_data(self, value: dict[str, any]) -> None:
         key = list(value.keys())[0]
 
@@ -38,3 +28,5 @@ class Metric:
 
     def get_metric_data(self)->list[dict]:
         return self._metric_data
+
+metric = Metric(data)
